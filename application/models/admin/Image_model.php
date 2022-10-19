0ebe49564f2bbbd93c21cd228647974836063bfc
Builtangular <monika23.mrd@gmail.com>
2022-10-19 12:17:22 +0530

    new changes

--- application/models/admin/Image_model.php
+++ application/models/admin/Image_model.php
@@ -20,12 +20,12 @@ class Image_model extends CI_Model {
         $result = $this->db->insert('tbl_image',$data);
         return  $result;
      }
-     public function update_image($id,$file)
+     public function update_image($image_id)
      {
          $update = array(
-                'image_file'=>$file,
+                'image_file'=>$this->input->post('image_file'),
              );
-         $this->db->where('id',$id);
+         $this->db->where('id',$image_id);
          return $this->db->update('tbl_image', $update);
      }
      function get_image_data($report_id)
